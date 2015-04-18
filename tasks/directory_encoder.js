9ee3b89506120c0821a6f7137eb39b16a342c76f
Tim Rourke <tim@timrourke.com>
2015-04-18 10:28:37 -0500

    Adding directory-encoder as dependency

---
+++ tasks/directory_encoder.js
@@ -0,0 +1,50 @@
+/*
+ * grunt-directory-encoder
+ * https://github.com/timrourke/grunt-directory-encoder
+ *
+ * Copyright (c) 2015 Tim Rourke
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('directory_encoder', "Filament Group's directory-encoder, as a grunt plugin. Takes a directory of images, turns it into some CSS.", function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
